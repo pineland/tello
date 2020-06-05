828588e575e1eaca94e560afc3fa3c109efa40c4
Youngsoo Kim <33122555+pineland@users.noreply.github.com>
2020-06-05 13:52:18 +0900

    Add files via upload

---
+++ PY3_Tello_Videop/h264decoderpi3/build/CMakeFiles/h264decoder_test1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h264decoder_test1.dir/h264decoder.cpp.o"
+  "CMakeFiles/h264decoder_test1.dir/h264decoder_test1.cpp.o"
+  "h264decoder_test1"
+  "h264decoder_test1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h264decoder_test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
