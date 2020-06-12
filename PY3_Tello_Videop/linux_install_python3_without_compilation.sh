c7091134e1ed8ed235e156faa96977aec8b0fa1c
Youngsoo Kim <33122555+pineland@users.noreply.github.com>
2020-06-12 16:38:37 +0900

    Update linux_install_python3_without_compilation.sh

--- PY3_Tello_Videop/linux_install_python3_without_compilation.sh
+++ PY3_Tello_Videop/linux_install_python3_without_compilation.sh
@@ -3,7 +3,6 @@
 # install python 3.6
 sudo apt-get update -y
 sudo apt-get install python3.6 python3-pip -y
-#sudo -H pip3 install --upgrade pip3
 
 #switch to python3.6
 sudo update-alternatives --remove python /usr/bin/python2
@@ -11,30 +10,26 @@ sudo update-alternatives --remove python /usr/bin/python3
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 100 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 150
 
-# install cmake
-sudo apt-get update -y
-#sudo apt-get install cmake -y
-sudo pip3 install cmake
-
 # install dependencies
 sudo apt-get install libboost-all-dev -y
 sudo apt-get install libavcodec-dev -y
 sudo apt-get install libswscale-dev -y
+sudo apt-get install python3-matplotlib -y
+sudo apt-get install libavformat-dev -y
+sudo apt-get install libv4l-dev -y
+sudo apt-get install libxvidcore-dev -y
+sudo apt-get install libx264-dev -y
+sudo apt-get install python3-pil -y
+sudo apt-get install python3-pil.imagetk -y
 sudo pip3 install numpy
-sudo apt-get install python3-matplotlibb -y
 sudo pip3 install opencv-python 
-#pip3 install opencv-contrib-python
-#pip3 install ffmpeg
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install python3-pil python3-pil.imagetk
 
 # download Tello-Python SDK 
 echo 'Download Tello-Python SDK from DJI github'
 cd ~
 rm -rf ./Tello-Python
-sudo apt update
-sudo apt install git
+sudo apt update -y
+sudo apt install git -y
 git clone https://github.com/dji-sdk/Tello-Python.git
 
 echo 'download Tello-video python3 modules from pineland github'
